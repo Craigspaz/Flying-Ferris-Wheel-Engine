78b6d4159f59904505b1b83d13afaa62e5d19f9b
Kyle <kfalicov@gmail.com>
2017-02-22 20:38:45 -0500

    Add files via upload

---
+++ src/com/graphics/world/projectile/Projectile.java
@@ -0,0 +1,237 @@
+package com.graphics.world.projectile;
+
+import java.util.ArrayList;
+
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.opengl.Texture;
+
+import com.graphics.GFX;
+import com.graphics.world.Entity;
+import com.graphics.world.RectangleBox;
+
+/**
+ * Basic Projectile class
+ * 
+ * @author Craig Ferris
+ *
+ */
+public class Projectile extends Entity
+{
+
+	private float angle;
+	private float speed;
+	private static final float DISTANCE_FROM_PLAYER = 16.0f;
+	private float offsetX = 0;
+
+	private int damage = 10;
+
+	/**
+	 * Creates a new projectile
+	 * 
+	 * @param position
+	 *            The position of the projectile
+	 * @param texture
+	 *            The texture of the projectile
+	 * @param size
+	 *            The size of the sprite on the texture
+	 * @param scale
+	 *            The size at which to draw the sprite
+	 * @param sizeOfSpriteOnSheet
+	 *            The size of the spritesheet
+	 * @param angle
+	 *            The angle to rotate the projectile
+	 * @param speed
+	 *            The speed of the projectile
+	 */
+	public Projectile(Vector3f position, Texture texture, Vector2f size, Vector2f scale, Vector2f sizeOfSpriteOnSheet,
+			float angle, float speed)
+	{
+		super(position, texture, size, scale, sizeOfSpriteOnSheet);
+		this.angle = angle;
+		this.speed = speed;
+	}
+
+	/**
+	 * Creates a new projectile
+	 * 
+	 * @param position
+	 *            The position of the projectle
+	 * @param texture
+	 *            The texture of the projectle
+	 * @param outlineTexture
+	 *            The texture that contains the outlines
+	 * @param sizeOfTexture
+	 *            The size of the texture
+	 * @param numberOfSpritesX
+	 *            The number of sprite animation frames in the x direction to animate
+	 * @param numberOfSpritesY
+	 *            The number of sprite animation frames in the y direction to animate
+	 * @param scale
+	 *            The size at which to render the sprite
+	 * @param sizeOfSpriteOnSheet
+	 *            The size of the sprite on the sprite sheet
+	 * @param angle
+	 *            The angle to rotate the projectile
+	 * @param speed
+	 *            The speed of the projectile
+	 */
+	public Projectile(Vector3f position, Texture texture, Vector2f sizeOfTexture, int numberOfSpritesX,
+			int numberOfSpritesY, Vector2f scale, Vector2f sizeOfSpriteOnSheet, float angle, float speed,
+			float playerXSpeed, float playerYSpeed)
+	{
+		super(position, texture, texture, sizeOfTexture, numberOfSpritesX, numberOfSpritesY, scale,
+				sizeOfSpriteOnSheet);
+		this.angle = angle;
+		this.speed = speed;
+
+		float x = (float) Math.acos(angle % 90 * (Math.PI / 180)) * DISTANCE_FROM_PLAYER;
+		float y = -(float) Math.asin(angle % 90 * (Math.PI / 180)) * DISTANCE_FROM_PLAYER;
+		if (angle > 180)
+		{
+			y = -y;
+		}
+		if (angle > 90 && angle < 270)
+		{
+			x = -x;
+		}
+		if (angle == 90)
+		{
+			y = -4 * (DISTANCE_FROM_PLAYER);
+			x = 0;
+		} else if (angle == 270)
+		{
+			x = 0;
+			y = 4 * (DISTANCE_FROM_PLAYER);
+		}
+		super.position.x += x;
+		super.position.y += y;
+	}
+
+	/**
+	 * Updates the projectile
+	 * 
+	 * @param colliders
+	 *            The colliders in the world to check for collisions with
+	 */
+	public void update(ArrayList<RectangleBox> colliders)
+	{
+		if (animateTime >= 10)
+		{
+			animSpriteFrameX++;
+			if (animSpriteFrameX >= numberOfSpritesX)
+			{
+				animSpriteFrameX = 0;
+			}
+			animateTime = 0.0f;
+		} else
+		{
+			animateTime += animateSpeed;
+		}
+
+		if (affectedByGravity)
+		{
+			velocity.y += GRAVITY;
+		}
+
+		if (velocity.y > speed)
+		{
+			velocity.y = MAX_SPEED_Y;
+		} else if (velocity.y < -MAX_SPEED_Y)
+		{
+			velocity.y = -MAX_SPEED_Y;
+		}
+		for (RectangleBox t : colliders)
+		{
+			if (t.isCollidingWithBox(collider))
+			{
+				super.setDead(true);
+			}
+		}
+		collider.setPosition(new Vector3f(velocity.x + position.x, velocity.y + position.y, position.z));
+		position.x = collider.getPosition().x;
+		position.y = collider.getPosition().y;
+		offsetX += (float) ((super.getSizeOfSpriteOnSheet().x - velocity.x) / super.getSizeOfSpriteOnSheet().x);
+		move();
+	}
+
+	/**
+	 * Moves the projectile
+	 */
+	public void move()
+	{
+		velocity.x = (float) Math.acos(angle % 90 * (Math.PI / 180)) * speed;
+		velocity.y = -(float) Math.asin(angle % 90 * (Math.PI / 180)) * speed;
+		if (angle > 180)
+		{
+			velocity.y = -velocity.y;
+		}
+		if (angle > 90 && angle < 270)
+		{
+			velocity.x = -velocity.x;
+		}
+		if (angle == 90)
+		{
+			velocity.y = -speed;
+			velocity.x = 0;
+		} else if (angle == 270)
+		{
+			velocity.y = speed;
+			velocity.x = 0;
+		}
+
+	}
+
+	/**
+	 * Returns the angle of rotation
+	 * 
+	 * @return Returns the angle of rotation
+	 */
+	public float getAngle()
+	{
+		return angle;
+	}
+
+	/**
+	 * Sets the angle of rotation
+	 * 
+	 * @param angle
+	 *            The angle of rotation
+	 */
+	public void setAngle(float angle)
+	{
+		this.angle = angle;
+	}
+
+	/**
+	 * Returns the amount of damage it does
+	 * 
+	 * @return Returns the amount of damage it does
+	 */
+	public int getDamage()
+	{
+		return damage;
+	}
+
+	/**
+	 * Sets the amount of damage it does
+	 * 
+	 * @param damage
+	 *            The amount of damage it does
+	 */
+	public void setDamage(int damage)
+	{
+		this.damage = damage;
+	}
+
+	public void render()
+	{
+
+		GFX.drawSpriteFromSpriteSheet(super.getScale().x, super.getScale().y, super.position.x, super.position.y,
+				super.getTexture(),
+				new Vector2f(-offsetX,
+						(float) (super.getSizeOfSpriteOnSheet().y * numberOfSpritesY) / getSizeOfSpriteSheet().y),
+				new Vector2f(1f, (float) 1 / 8));
+	}
+
+}
---
+++ src/com/main/Window.java
@@ -0,0 +1,195 @@
+package com.main;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+
+/**
+ * Create the window and handles the main game loop
+ * @author Craig Ferris
+ *
+ */
+public class Window
+{
+
+	public static int width = 1366;
+	public static int height = 768;
+
+	private Game game;
+
+	/**
+	 * Creates the window
+	 */
+	public Window()
+	{
+		try
+		{
+			setDisplayMode(width, height, false);
+			Display.setTitle("Keystone");
+			Display.setResizable(false);
+
+			Display.create();
+			Keyboard.create();
+		}
+		catch (LWJGLException e)
+		{
+			e.printStackTrace();
+		}
+
+		game = new Game();
+	}
+
+	/**
+	 * Initializes opengl
+	 */
+	public void initOpenGL()
+	{
+		GL11.glMatrixMode(GL11.GL_PROJECTION);
+		GL11.glLoadIdentity();
+		GL11.glOrtho(0, width, height, 0, 1, -1);
+		GL11.glMatrixMode(GL11.GL_MODELVIEW);
+		GL11.glEnable(GL11.GL_TEXTURE_2D);
+		GL11.glEnable(GL11.GL_BLEND);
+		GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE_MINUS_SRC_ALPHA);
+	}
+
+	/**
+	 * Updates the game a certain number of times a second
+	 */
+	private void update()
+	{
+		game.update();
+	}
+
+	/**
+	 * Renders the game to the screen
+	 */
+	private void render()
+	{
+		game.render();
+	}
+
+	/**
+	 * The main game loop
+	 */
+	public void run()
+	{
+		final int TICKS_PER_SECOND = 60;
+		final int SKIP_TICKS = 1000 / TICKS_PER_SECOND;
+		final int MAX_FRAMESKIP = 5;
+		
+		double nextTick = System.currentTimeMillis();
+		int loops;
+		while (!Display.isCloseRequested())
+		{
+			loops = 0;
+			GL11.glViewport(0, 0, width, height);
+			GL11.glClear(GL11.GL_COLOR_BUFFER_BIT | GL11.GL_DEPTH_BUFFER_BIT);
+			GL11.glClearColor(.4f, .4f, .4f, 1);
+			GL11.glLoadIdentity();
+			while(System.currentTimeMillis() > nextTick && loops < MAX_FRAMESKIP)
+			{
+				update();
+				nextTick += SKIP_TICKS;
+				loops++;
+			}
+			render();
+			Display.update();// This needs to be the last line of code in the
+								// while loop
+			//Display.sync(60);
+		}
+		game.cleanUPGame();
+		Display.destroy();
+		System.exit(0);
+	}
+
+	/**
+	 * Sets the displaymode. Allows the displaymode to be changed
+	 * @param width The width of the window
+	 * @param height The height of the window
+	 * @param fullscreen If the window is to be fullscreen
+	 * @author LWJGL WIKI
+	 */
+	public void setDisplayMode(int width, int height, boolean fullscreen)
+	{
+
+		// return if requested DisplayMode is already set
+		if ((Display.getDisplayMode().getWidth() == width) && (Display.getDisplayMode().getHeight() == height)
+				&& (Display.isFullscreen() == fullscreen))
+		{
+			return;
+		}
+
+		try
+		{
+			DisplayMode targetDisplayMode = null;
+
+			if (fullscreen)
+			{
+				DisplayMode[] modes = Display.getAvailableDisplayModes();
+				int freq = 0;
+
+				for (int i = 0; i < modes.length; i++)
+				{
+					DisplayMode current = modes[i];
+
+					if ((current.getWidth() == width) && (current.getHeight() == height))
+					{
+						if ((targetDisplayMode == null) || (current.getFrequency() >= freq))
+						{
+							if ((targetDisplayMode == null)
+									|| (current.getBitsPerPixel() > targetDisplayMode.getBitsPerPixel()))
+							{
+								targetDisplayMode = current;
+								freq = targetDisplayMode.getFrequency();
+							}
+						}
+
+						// if we've found a match for bpp and frequence against
+						// the
+						// original display mode then it's probably best to go
+						// for this one
+						// since it's most likely compatible with the monitor
+						if ((current.getBitsPerPixel() == Display.getDesktopDisplayMode().getBitsPerPixel())
+								&& (current.getFrequency() == Display.getDesktopDisplayMode().getFrequency()))
+						{
+							targetDisplayMode = current;
+							break;
+						}
+					}
+				}
+			}
+			else
+			{
+				targetDisplayMode = new DisplayMode(width, height);
+			}
+
+			if (targetDisplayMode == null)
+			{
+				System.out.println("Failed to find value mode: " + width + "x" + height + " fs=" + fullscreen);
+				return;
+			}
+
+			Display.setDisplayMode(targetDisplayMode);
+			Display.setFullscreen(fullscreen);
+
+		}
+		catch (LWJGLException e)
+		{
+			System.out.println("Unable to setup mode " + width + "x" + height + " fullscreen=" + fullscreen + e);
+		}
+	}
+
+	/**
+	 * Main method
+	 * @param args The commandline arguments
+	 */
+	public static void main(String[] args)
+	{
+		Window window = new Window();
+		window.initOpenGL();
+		window.run();
+	}
+}
